c425cea9d40aa2109b078f62032df81725cae6ec
Raman Liausha <Raman_Liausha@epam.com>
2022-09-29 19:33:26 +0300

    fix: change path

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -2,6 +2,6 @@ export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	bff: 'https://rhxhqqtz27.execute-api.eu-west-1.amazonaws.com',
+	bff: 'https://okb6diqh36.execute-api.eu-west-1.amazonaws.com',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
